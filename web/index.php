5e4010d69310007567e8410fdd5e4ed6e2e86ad6
cwjyun <cwjyun@163.com>
2018-11-21 18:11:22 +0800

    111111

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-$config = require __DIR__ . '/../config/web.php';
+;
+$config = require __DIR__ . '/../config/'.YII_ENV.'/'.YII_ENV.'_web.php';
 
 (new yii\web\Application($config))->run();

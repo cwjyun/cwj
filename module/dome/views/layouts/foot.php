b59efe366b4e2fb176aab1d092fec9050e8f9645
cwjyun <cwjyun@163.com>
2018-11-22 17:01:19 +0800

    前段代码上传

---
+++ module/dome/views/layouts/foot.php
@@ -0,0 +1,7 @@
+<div id="templatemo_copyright_wrapper">
+    <div id="templatemo_copyright">
+
+        Copyright © 2048 <a href="#">Your Company Name</a> | Template from <a href="http://www.cssmoban.com" target="_parent" title="网站模板">网站模板</a>
+
+    </div> <!-- end of templatemo_footer -->
+</div>
---
+++ module/dome/views/layouts/main.php
@@ -0,0 +1,8 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
+<?php include 'header.php';?>
+<?= $content ?>
+<?php include 'foot.php';?>

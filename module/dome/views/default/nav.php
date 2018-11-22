f2fb2888f746d22fb67560dd200729a18f4c49f2
cwjyun <cwjyun@163.com>
2018-11-22 17:10:21 +0800

    前段代码上传

--- module/dome/views/default/nav.php
+++ module/dome/views/default/nav.php
@@ -2,11 +2,11 @@
     <div id="site_title"><h1><a href="#">开心就好</a></h1></div>
     <div id="templatemo_menu">
         <ul>
-            <li><a href="index.html" class="current">Home</a></li>
-            <li><a href="about.html">About Us</a></li>
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="gallery.html">Gallery</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="index.html" class="current">首页</a></li>
+            <li><a href="about.html">各类软件安装</a></li>
+            <li><a href="blog.html">心得</a></li>
+            <li><a href="gallery.html">想象中</a></li>
+            <li><a href="contact.html">想象中</a></li>
         </ul>
     </div> <!-- end of templatemo_menu -->
 </div> <!-- end of header -->

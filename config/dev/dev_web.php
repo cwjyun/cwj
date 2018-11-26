ca3dba28ede3adb69a4d540e00f7a7c1d1b073f5
cwjyun <cwjyun@163.com>
2018-11-26 16:22:07 +0800

    前段代码上传

--- config/dev/dev_web.php
+++ config/dev/dev_web.php
@@ -43,7 +43,7 @@ $config = [
             'appId' => 'wxf36cf24b286581f5',
             'appSecret' => '950b0ff63524aafd7d385dc9e27df8f1',
             'token' => '',
-            'wechatRedirect' => 'http://cwj.test/get-weixin-code.html?',
+            'wechatRedirect' => 'http://47.93.246.251/get-weixin-code.html?',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',

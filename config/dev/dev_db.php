6eb6e0fd11f7fc6f21ea7b7d82ed0682644b345d
cwjyun <cwjyun@163.com>
2018-11-27 15:15:43 +0800

    前段代码上传

--- config/dev/dev_db.php
+++ config/dev/dev_db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=wechat',
     'username' => 'root',
     'password' => '3344134',
     'charset' => 'utf8',

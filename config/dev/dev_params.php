8ac7f1cacfd8e3fcf69996e5b2dbead19c6a0c43
cwjyun <cwjyun@163.com>
2018-11-28 11:42:55 +0800

    前段代码上传

--- config/dev/dev_params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'wechat' =>[
-        'token' => 'wx378d3395a6f41442',
-    ],
-];
--- module/dome/views/layouts/main.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* @var $this \yii\web\View */
-/* @var $content string */
-?>
-<?php include 'header.php';?>
-<?= $content ?>
-<?php include 'foot.php';?>

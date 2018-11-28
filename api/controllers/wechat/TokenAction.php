ad025710410cbe55f7bb273f7d6a55d285ef6d96
cwjyun <cwjyun@163.com>
2018-11-28 19:08:44 +0800

    前段代码上传

--- api/controllers/wechat/TokenAction.php
+++ api/controllers/wechat/TokenAction.php
@@ -5,7 +5,8 @@ namespace app\controllers\wechat;
 use Yii;
 use yii\rest\Action;
 
-class TokenAction extends Action {
+class TokenAction extends Action
+{
     /**
      * @param $ppid
      * @param string $type
@@ -13,18 +14,53 @@ class TokenAction extends Action {
      * @param string $consume_id
      * @param string $consume_name
      */
-    public function run()
+    public function run($echostr, $signature, $timestamp, $nonce)
     {
         try {
+            $this->valid([
+                'echostr' => $echostr,
+                'signature' => $signature,
+                'timestamp' => $timestamp,
+                'nonce' => $nonce
+            ]);
 
-            die("成功调用接口");
-            
         } catch (\Exception $e) {
 
         }
     }
 
+    public function valid($get)
+    {
+        $echoStr = $get["echostr"];
+        $signature = $get["signature"];
+        $timestamp = $get["timestamp"];
+        $nonce = $get["nonce"];
+        //valid signature , option
+        if ($this->checkSignature($signature, $timestamp, $nonce)) {
+            echo $echoStr;
+        }
+    }
 
+    private function checkSignature($signature, $timestamp, $nonce)
+    {
+        // you must define TOKEN by yourself
+        $token = Yii::$app->params['wechat']['token'];
+        if (!$token) {
+            echo 'TOKEN is not defined!';
+        } else {
+            $tmpArr = array($token, $timestamp, $nonce);
+            // use SORT_STRING rule
+            sort($tmpArr, SORT_STRING);
+            $tmpStr = implode($tmpArr);
+            $tmpStr = sha1($tmpStr);
+
+            if ($tmpStr == $signature) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
 
 }

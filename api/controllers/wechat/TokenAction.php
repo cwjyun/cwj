b94b4001921de01b886b019c8c557c0096c95002
cwjyun <cwjyun@163.com>
2018-11-28 20:25:43 +0800

    前段代码上传

--- api/controllers/wechat/TokenAction.php
+++ api/controllers/wechat/TokenAction.php
@@ -5,6 +5,7 @@ namespace app\controllers\wechat;
 use Yii;
 use yii\rest\Action;
 
+
 class TokenAction extends Action
 {
     /**
@@ -14,51 +15,16 @@ class TokenAction extends Action
      * @param string $consume_id
      * @param string $consume_name
      */
-    public function run($echostr, $signature, $timestamp, $nonce)
+    public function run($signature, $timestamp, $nonce)
     {
         try {
-            $get = [
-                'echostr' => $echostr,
-                'signature' => $signature,
-                'timestamp' => $timestamp,
-                'nonce' => $nonce
-            ];
-            $this->valid($get);
-
-        } catch (\Exception $e) {
-
-        }
-    }
-
-    public function valid($get)
-    {
-        $echoStr = $get["echostr"];
-        $signature = $get["signature"];
-        $timestamp = $get["timestamp"];
-        $nonce = $get["nonce"];
-        //valid signature , option
-        if ($this->checkSignature($signature, $timestamp, $nonce)) {
-            echo $echoStr;
-        }
-    }
-
-    private function checkSignature($signature, $timestamp, $nonce)
-    {
-        // you must define TOKEN by yourself
-        $token = Yii::$app->params['wechat']['token'];
-        if (!$token) {
-            echo 'TOKEN is not defined!';
-        } else {
-            $tmpArr = array($token, $timestamp, $nonce);
-            // use SORT_STRING rule
-            sort($tmpArr, SORT_STRING);
-            $tmpStr = implode($tmpArr);
-            $tmpStr = sha1($tmpStr);
-            if ($tmpStr == $signature) {
-                return true;
-            } else {
+            if (!Yii::$app->wechat->checkSignature($signature, $timestamp, $nonce)) {
                 return false;
             }
+            return true;
+
+        } catch (\Exception $e) {
+            var_dump($e);
         }
     }
 

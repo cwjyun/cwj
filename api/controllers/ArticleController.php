7a00e7da04998bd4350d7d7ccf26b0e46c5d9c3d
cwjyun <cwjyun@163.com>
2018-11-28 17:12:31 +0800

    前段代码上传

--- api/controllers/ArticleController.php
+++ api/controllers/ArticleController.php
@@ -1,15 +1,23 @@
 <?php
+
 namespace api\controllers;
+
 use yii\rest\ActiveController;
 use common\models\Article;
-/**
 
+/**
  */
 class ArticleController extends ActiveController
 {
-    
-    public $modelClass='\common\models\Article';
 
-    public function init(){
+    public $modelClass = '\common\models\Article';
+
+    public function init()
+    {
+    }
+
+    public function actionIndex()
+    {
+
     }
 }

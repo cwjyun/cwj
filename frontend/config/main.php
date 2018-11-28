691600760b9b1c51d9ac4a7fbb48ea3d842ebfaa
cwjyun <cwjyun@163.com>
2018-11-28 15:51:53 +0800

    前段代码上传

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,7 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'defaultRoute'=>'my/index',//默认路由，控制器+方法
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',

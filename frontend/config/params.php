d1aca2292d6656c5bf47d8eaaaec5602a3219b60
cwjyun <cwjyun@163.com>
2018-11-29 14:14:27 +0800

    前段代码上传

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -4,5 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'wechat_api'=>[
         'getToken'=>WECHAT_API_URL.'/wechat/token',
+        'get_opid' =>WECHAT_API_URL.'/wechat/get_opid',
     ]
 ];

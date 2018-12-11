90e1d806619c4555e8fcde63e86470c80f31904e
cwjyun <cwjyun@163.com>
2018-12-11 10:39:41 +0800

    上传上个星期干的破事

--- frontend/common/widgets/views/footer.php
+++ frontend/common/widgets/views/footer.php
@@ -9,7 +9,7 @@
                 <p>Copyright &copy; 2014.Company name All rights reserved.<a target="_blank" href="http://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a></p>
             </div>
             <div class="span4">
-                <a href="index.html"><?= Html::img('@web/assets/img/logo2-default.png',['id'=>'logo-footer','class'=>'pull-right'])?></a>
+                <a href="index.html"><?= Html::img('@path_root/assets/img/logo2-default.png',['id'=>'logo-footer','class'=>'pull-right'])?></a>
             </div>
         </div><!--/row-fluid-->
     </div><!--/container-->

3b070350156269a2b669c4b1bb792aa74552a425
cwjyun <cwjyun@163.com>
2018-12-13 14:40:49 +0800

    上传上个星期干的破事

--- backend/controllers/AdminController.php
+++ backend/controllers/AdminController.php
@@ -12,15 +12,39 @@ use yii\web\User;
 class AdminController extends BaseController
 {
 
+    /**
+     * 后台首页
+     * @return string
+     */
     public function actionIndex()
     {
         return $this->render('index');
     }
 
+    /**
+     * 配置信息页面
+     * @return string
+     */
     public function actionWelcome()
     {
         $mysql_info = Yii::$app->cwj_db->createCommand("select version()")->queryOne();
         $data['mysql_info']['banben'] = $mysql_info['version()'];
-        return $this->render('welcome',['data'=>$data]);
+        return $this->render('welcome', ['data' => $data]);
     }
+
+
+    /**
+     * 文章导航管理页面
+     * @return string
+     */
+    public function actionCate()
+    {
+        return $this->render('cate');
+    }
+
+
+    public function actionEditcate(){
+        return $this->render('edit_cate');
+    }
+
 }
